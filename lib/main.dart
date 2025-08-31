54f668da6b244fd7de56b9d7e9bedb787c826d1a
HerizoRand <hontyherizorandrianarisoa@yahoo.com>
2025-08-31 12:32:26 +0300

    Upadate main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -20,28 +21,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DiceApp extends StatelessWidget {
+class DiceApp extends StatefulWidget {
   const DiceApp({super.key});
 
   @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+    return TextButton(
+      style: TextButton.styleFrom(overlayColor: Colors.transparent),
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
+              ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            Expanded(
+              child: TextButton(
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  changeDiceFace();
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      onPressed: () {
+        setState(() {
+          leftDiceNumber = Random().nextInt(6) + 1;
+          rightDiceNumber = Random().nextInt(6) + 1;
+        });
+      },
     );
   }
 }
